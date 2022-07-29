653b8e81942a5fd598851746301b575708d73c21
landruu <andres030685@gmail.com>
2022-07-29 01:17:20 -0300

    Conectando re-configurando urls

--- src/app/topBar/top-bar/top-bar.component.ts
+++ src/app/topBar/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 
 @Component({
@@ -8,13 +9,22 @@ import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 })
 export class TopBarComponent implements OnInit {
 
-  constructor(private isLogged: AutenticacionService) { }
+  constructor(private isLogged: AutenticacionService, private rutear: Router) { }
 
   ngOnInit(): void {
   }
 
   onAdmin(): Boolean {
+    this.goStart();
     return this.isLogged.isUserLogged();
+  }
+
+  NoAdmin() {
+    this.isLogged.logout();
+    this.goStart();
+  }
+  goStart(){
+    this.rutear.navigate(['']);
   }
 
 }
